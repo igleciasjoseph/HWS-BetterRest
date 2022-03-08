a64288e7e9fadb6cbdfacc3aa8594dad1ac6fa9e
Joseph Iglecias <igleciasjoseph@gmail.com>
2022-03-08 13:40:25 -0500

    Initial Commit

---
+++ HWS-BetterRest/HWS_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_BetterRestApp.swift
+//  HWS-BetterRest
+//
+//  Created by Joseph Iglecias on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct HWS_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
